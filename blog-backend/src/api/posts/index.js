eeff5588a385db2bf971628a21ee157a2ab8a7dc
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-06-11 11:35:25 +0900

    노드.디비 연결 서버

---
+++ blog-backend/src/api/posts/index.js
@@ -0,0 +1,17 @@
+import Router from "koa-router";
+import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from "../../lib/checkLoggedIn"
+
+
+const posts=new Router();
+
+
+
+posts.get('/',postsCtrl.list);
+posts.post('/',checkLoggedIn,postsCtrl.write);
+posts.get("/:id",postsCtrl.read);
+posts.delete("/:id",checkLoggedIn,postsCtrl.remove);
+posts.patch("/:id",checkLoggedIn,postsCtrl.update);
+
+
+export default posts;
---
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,80 @@
+import Post from "../../models/post"
+
+export const write =async ctx =>{
+    const{title,body,tags}=ctx.request.body;
+    const post=new Post({
+        title,
+        body,
+        tags,
+        user:ctx.status.user,
+    });
+    try{
+        await post.save();
+        ctx.body=post;
+    }catch(e){
+        ctx.throw(500,e)
+    }
+};
+
+
+
+export const list=async ctx=>{
+try{
+    const posts=await Post.find().exec();
+    ctx.body=posts;
+}catch(e){
+    ctx.throw(500,e);
+}
+
+}
+
+export const read= async ctx=>{
+const {id} =ctx.params;
+try{
+    const post=await Post.findById(id).exec();
+    if(!post){
+        ctx.status=404;
+        return;
+    }
+    ctx.body=post;
+}catch(e){
+    ctx.throw(500,e)
+}
+
+
+}
+
+export const remove= async ctx=>{
+    const {id}=ctx.params;
+    try{
+        await Post.findByIdAndRemove(id).exec();  //해당 아이디 삭제
+        ctx.status=204;  //No Content(성공하기는 했지만 응답할 데이터는 없음)
+    }catch (e){
+        ctx.throw(500,exports)
+    }
+
+}
+
+
+//findByIdAndUpdate ㅎ마수를 사용할때는 세가지 파라미터를 주어야 한다
+//첫번째 파라미터는 id
+//두번째 파라미터는 업데이트 내용
+//세번째 파라미터는 업데이트의 옵션
+
+export const update=async ctx=>{
+    const{id}=ctx.params;
+    try{
+        const post=await Post.findByIdAndUpdate(id,ctx.request.body,{
+            new:true, //이 값을설정하면 업데이트된 데이터를 반환한다.
+        }).exec();
+        if(!post){
+            ctx.status=404;
+            return;
+        }
+        ctx.body=post;
+
+    }catch (e){
+        ctx.throw(500,e)
+    }
+
+}
